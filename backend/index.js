8868cd9707b1a0beee5ffc55e03f7e102dce6619
sohal13 <sohalr1305@gmail.com>
2024-02-21 15:42:09 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from 'dotenv'
 import cookieparser from "cookie-parser"
@@ -9,7 +10,9 @@ import messageRout from './rout/message.route.js'
 
 import { app, server } from "./socket/socket.js"
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,7 +27,11 @@ app.use('/api/auth',authRout)
 app.use('/api/message',messageRout)
 app.use('/api/user',userRout)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(3000,()=>{
     dbConnection(),
