5c2cd88103399a455517b2bc9d110a97c369afad
sohal13 <sohalr1305@gmail.com>
2024-03-01 17:06:43 +0530

    new chnages

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -21,7 +21,14 @@ const Home = () => {
     setIsSidebarVisible(!selectedUser); 
   }, [selectedUser]);
   return (
-    <div className='flex justify-between min-w-full md:min-w-[550px] md:max-w-[65%] px-2 h-[95%] md:h-full  rounded-xl shadow-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex justify-between min-w-full
+     md:min-w-[550px] md:max-w-[65%]
+      px-2 h-[95%] md:h-full  
+      rounded-xl shadow-lg
+       bg-gray-400 bg-clip-padding
+        backdrop-filter backdrop-blur-lg 
+        bg-opacity-0'
+        >
       <div className={`w-full py-2 md:flex ${isSidebarVisible ? '' : 'hidden'}`}>
       <Sidebar onSelectUser={handleUserSelect}/>
       </div>
